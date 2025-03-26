743cb2235c7f29650b195ce87b81ccd17b313735
DeepakPachipala <113615147+DeepakPachipala@users.noreply.github.com>
2025-03-26 13:15:20 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const { db } = require('./db/db');
 const {readdirSync} = require('fs')
 const app = express()
+const path = './routes';
 
 require('dotenv').config()
 
@@ -14,8 +15,14 @@ app.use(express.json())
 app.use(cors({ origin: frontendUrl }))
 
 //routes
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
-
+if (fs.existsSync(path)) {
+  console.log("Routes directory exists!");
+  readdirSync(path).map((route) =>
+    app.use("/api/v1", require(`./routes/${route}`))
+  );
+} else {
+  console.error(`${path} does not exist.`);
+}
 const server = () => {
     db()
     app.listen(PORT, () => {
