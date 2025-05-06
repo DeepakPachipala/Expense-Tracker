8ac325ddf3a65320cdc0e8c424ca2de99fdb2e28
DeepakPachipala <deepakpachipala@gmail.com>
2025-05-06 18:08:51 +0530

    changes

--- backend/models/ExpenseModel.js
+++ backend/models/ExpenseModel.js
@@ -23,11 +23,11 @@ const ExpenseSchema = new mongoose.Schema({
         required: true,
         trim: true
     },
-    // category: {
-    //     type: String,
-    //     required: true,
-    //     trim: true
-    // },
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
     description: {
         type: String,
         required: true,
