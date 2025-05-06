590d176bc5c1dcfcc2b0c381adefde598f78add2
DeepakPachipala <113615147+DeepakPachipala@users.noreply.github.com>
2025-05-06 15:12:14 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 5001
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 
 const path = require('path');
 
